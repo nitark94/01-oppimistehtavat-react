f3de8a03b2d3b10bd0c0252871b2281359e9de18
Ranta-Knuuttila Nita <s2401565@BC-TG0W7DYQVW-MacBook-Air.local>
2025-01-22 10:28:36 +0200

    Tehtäviä tehty eteenpäin

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,19 @@
-const Hello = (props) => {
-  console.log(props)
+const Hello = ({ name, age }) => {
+  const { name, age } = props
+  const bornYear = () => new Date().getFullYear() - age
+
+
   return (
     <>
       <p>Hello {props.name}, you are {props.age} years old</p>
+    <p>So you were probably born {bornYear()}</p>
     </>
   )
 }
 const App = () => {
   const nimi = 'Pekka'
   const ika = 10
+
   return (
     <>
       <h1>Greetings</h1>
