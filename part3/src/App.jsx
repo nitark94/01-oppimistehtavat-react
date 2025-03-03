805d978af234063be6f2895933c1cd22a843ffdf
nitark94 <nita.rantaknuuttila@gmail.com>
2025-03-03 12:53:48 +0200

    Valmis part3

--- part3/src/App.jsx
+++ part3/src/App.jsx
@@ -10,10 +10,22 @@ const Button = ({ onClick, text }) => {
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  console.log('rendering with counter value', counter);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter);
+    setCounter(counter + 1);
+  };
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter);
+    setCounter(counter - 1);
+  };
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter);
+    setCounter(0);
+  };
 
   return (
     <div>
